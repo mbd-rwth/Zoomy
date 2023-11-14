431587d13c4b97a492eb8331f185522552fcb6a2
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2023-11-14 16:08:42 +0100

    test reconstruction

---
+++ tests/unittests/test_flux.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pytest
+from types import SimpleNamespace
+
+from library.model import *
+import library.flux as num_flux
+import library.reconstruction as reconstruction
+
+
+@pytest.mark.critical
+@pytest.mark.unfinished
+@pytest.mark.parametrize(
+    "dimension",
+    ([1, 2]),
+)
+def test_default(dimension):
+    momentum_eqns = [[0], [0, 1]]
+    (
+        mesh,
+        model,
+        Q,
+        Qaux,
+        parameters,
+        num_normals,
+        normals,
+    ) = create_default_mesh_and_model(
+        dimension, Model, dimension, 0, 0, momentum_eqns[dimension - 1]
+    )
+
+    functions = model.get_runtime_model()
+    _ = model.create_c_interface()
+    c_model = model.load_c_model()
+
+    # get Lax-Friedrichs flux
+    F = num_flux.LF
+
+    # Reconstruction
+    Qi, Qj, Qauxi, Qauxj, normals_ij = reconstruction.constant(mesh, Q, Qaux)
+
+    mesh_props = SimpleNamespace(dt_dx=0.0)
+    flux = F(
+        Qi, Qj, Qauxi, Qauxj, parameters, normals_ij, c_model, mesh_props=mesh_props
+    )[0]
+
+    # Add flux (edges) to elements
+    (
+        map_elements_to_edges_plus,
+        map_elements_to_edges_minus,
+    ) = reconstruction.create_map_elements_to_edges(mesh)
+    Qnew = np.zeros_like(Q)
+    for i, elem in enumerate(map_elements_to_edges_plus):
+        Qnew[elem] += flux[i]
+    for i, elem in enumerate(map_elements_to_edges_minus):
+        Qnew[elem] -= flux[i]
+    if dimension == 1:
+        assert(np.allclose(Qnew.flatten(), np.array([-1.5,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,9.5,0.0,0.0])))
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    test_default(1)
+    test_default(2)
---
+++ tests/unittests/test_reconstruction.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pytest
+from types import SimpleNamespace
+
+from library.model import *
+import library.reconstruction as reconstruction
+
+@pytest.mark.critical
+def test_recon_const_1d():
+    dimension =1
+    momentum_eqns = [0]
+    (
+        mesh,
+        model,
+        Q,
+        Qaux,
+        parameters,
+        num_normals,
+        normals,
+    ) = create_default_mesh_and_model(dimension, Model, dimension, 0, 0, momentum_eqns)
+
+    recon = reconstruction.constant()
+    
+    Qi, Qj, Qauxi, Qauxj, normals_ij = recon(mesh, Q, Qaux)
+    # test inner domain
+    assert(np.allclose(Q[:9], Qi[:9]))
+    assert(np.allclose(Q[1:10], Qj[:9]))
+    # test boundary (Wall)
+    assert(np.allclose(-Q[0], Qj[9]))
+    assert(np.allclose(-Q[9], Qj[10]))
+
+@pytest.mark.critical
+@pytest.mark.unfinished
+def test_recon_const_2d():
+    dimension =2
+    momentum_eqns = [0, 1]
+    (
+        mesh,
+        model,
+        Q,
+        Qaux,
+        parameters,
+        num_normals,
+        normals,
+    ) = create_default_mesh_and_model(dimension, Model, dimension, 0, 0, momentum_eqns)
+
+    recon = reconstruction.constant()
+    
+    Qi, Qj, Qauxi, Qauxj, normals_ij = recon(mesh, Q, Qaux)
+
+@pytest.mark.critical
+@pytest.mark.unfinished
+def test_edges_to_elements_1d():
+    dimension =1
+    momentum_eqns = [0]
+    (
+        mesh,
+        model,
+        Q,
+        Qaux,
+        parameters,
+        num_normals,
+        normals,
+    ) = create_default_mesh_and_model(dimension, Model, dimension, 0, 0, momentum_eqns)
+
+    recon = reconstruction.constant()
+    
+    Qi, Qj, Qauxi, Qauxj, normals_ij = recon(mesh, Q, Qaux)
+
+    map_elements_to_edges_plus, map_elements_to_edges_minus = reconstruction.create_map_elements_to_edges(mesh)
+
+    Qnew = np.zeros_like(Q)
+    Qnew2 = np.zeros_like(Q)
+
+    Qnew[map_elements_to_edges_plus] += Qi
+    Qnew2[map_elements_to_edges_minus] += Qj
+    # # test inner domain
+    # assert(np.allclose(Q[:9], Qi[:9]))
+    # assert(np.allclose(Q[1:10], Qj[:9]))
+    # # test boundary (Wall)
+    # assert(np.allclose(-Q[0], Qj[9]))
+    # assert(np.allclose(-Q[9], Qj[10]))
+
+@pytest.mark.critical
+@pytest.mark.future
+def test_edges_to_elements_2d():
+    pass
+
+
+if __name__ == "__main__":
+    test_recon_const_1d()
+    test_recon_const_2d()
+    test_edges_to_elements_1d()
+    test_edges_to_elements_2d()
