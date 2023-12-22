2a8bd8a979f25e3f59729d105a65593a8b7505b4
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2023-12-22 12:30:55 +0100

    cleanup solver.py

--- library/solver.py
+++ library/solver.py
@@ -40,14 +40,8 @@ def initialize_problem(model, mesh):
 
     Q = np.empty((n_all_elements, n_fields), dtype=float)
     Qaux = np.zeros((Q.shape[0], model.aux_variables.length()))
-    # num_normals = mesh.element_n_neighbors
-    # normals = np.array(
-    #     [mesh.element_edge_normal[:, i] for i in range(mesh.n_nodes_per_element)]
-    # )
 
     model.initial_conditions.apply(mesh.element_center, Q[:mesh.n_elements])
-    # model.boundary_conditions.apply(Q)
-    # return Q, Qaux, normals
     return Q, Qaux
 
 def _get_compute_max_abs_eigenvalue(mesh, runtime_model, boundary_conditions, settings):
@@ -68,7 +62,6 @@ def _get_compute_max_abs_eigenvalue(mesh, runtime_model, boundary_conditions, se
                 # reconstruct 
                 [Qi, Qauxi], [Qj, Qauxj] = reconstruction(mesh, [Q, Qaux], i_elem, i_th_neighbor)
 
-                #TODO callout to a requirement of the flux
                 runtime_model.eigenvalues(
                     Qi, Qauxi, parameters, mesh.element_face_normals[i_elem, i_face], eigenvalues_i
                 )
@@ -100,7 +93,7 @@ def _get_compute_max_abs_eigenvalue(mesh, runtime_model, boundary_conditions, se
     return compute_max_abs_eigenvalue
 
 
-def get_semidiscrete_solution_operator_new(mesh, runtime_model, boundary_conditions, settings):
+def get_semidiscrete_solution_operator(mesh, runtime_model, boundary_conditions, settings):
     num_flux = settings.num_flux
     reconstruction = settings.reconstruction
     reconstruction_edge = settings.reconstruction_edge
@@ -151,43 +144,12 @@ def get_semidiscrete_solution_operator_new(mesh, runtime_model, boundary_conditi
     return solution_operator
     
 
-def get_semidiscrete_solution_operator(mesh, runtime_model, settings, on_edges_normal, on_edges_length, map_elements_to_edges):
-    num_flux = settings.num_flux
-    reconstruction = settings.reconstruction
-    # on_edge_incircle = recon.constant(mesh, [mesh.element_incircle])
-    def solution_operator(dt, Q, Qaux, parameters, dQ):
-        # Qi, Qj, Qauxi, Qauxj = reconstruction(mesh, Q, Qaux)
-        [Qi, Qauxi], [Qj, Qauxj] = reconstruction(mesh, [Q, Qaux])
-
-        # callout to a requirement of the flux
-        mesh_props = SimpleNamespace(dt_dx= dt / (0.1*on_edges_length))
-        flux, failed = num_flux(
-            Qi, Qj, Qauxi, Qauxj, parameters, on_edges_normal, runtime_model, mesh_props=mesh_props
-        )
-        assert not failed
-
-        # Add flux (edges) to elements
-        (
-            map_elements_to_edges_plus,
-            map_elements_to_edges_minus,
-        ) = map_elements_to_edges
-        for i, elem in enumerate(map_elements_to_edges_plus):
-            #TODO if hack: element_volume of of bounds error
-            if elem < mesh.n_elements:
-                dQ[elem] += flux[i] * on_edges_length[i] / mesh.element_volume[elem]
-        for i, elem in enumerate(map_elements_to_edges_minus):
-            if elem < mesh.n_elements:
-                dQ[elem] -= flux[i] * on_edges_length[i] / mesh.element_volume[elem]
-        return dQ
-    return solution_operator
-
 def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
     iteration = 0
     time = 0.0
 
     assert model.dimension == mesh.dimension
-    # self.reinitialize()
-    # self.solver.initialize(model=self.model, mesh=self.mesh)
+
     progressbar = pyprog.ProgressBar(
         "{}: ".format(settings.name),
         "\r",
@@ -195,8 +157,8 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
         complete_symbol="█",
         not_complete_symbol="-",
     )
-    progressbar.progress_explain = ""
-    progressbar.update()
+    # progressbar.progress_explain = ""
+    # progressbar.update()
     # force=True is needed in order to disable old logging root handlers (if multiple test cases are run by one file)
     # otherwise only a logfile will be created for the first testcase but the log file gets deleted by rmtree
     # logging.basicConfig(
@@ -209,62 +171,20 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
     # )
     # logger = logging.getLogger(__name__ + ":solve_steady")
 
-    # if model.dimension > 1 and self.cfl > 0.5:
-    #     logger.error(
-    #         "CFL = {} > 1/Dimension = 1/{}".format(self.cfl, self.model.dimension)
-    #     )
-
-    # Qnew = initial_condition.initialize(
-    #     self.model.initial_conditions,
-    #     self.model.n_fields,
-    #     self.mesh.element_center,
-    # )
-    # Q, Qaux, normals = initialize_problem(model, mesh)
     Q, Qaux = initialize_problem(model, mesh)
     parameters = model.parameter_values
     Qnew = deepcopy(Q)
 
-    # dt = self.dtmin
-    # kwargs = {
-    #     "model": self.model,
-    #     "callback_parameters": self.callback_parameters,
-    #     "aux_fields": {},
-    #     "mesh": self.mesh,
-    #     "time": time,
-    #     "iteration": iteration,
-    # }
-    # self.init_output_timesteps()
     # for callback in self.callback_function_list_init:
     #     Qnew, kwargs = callback(self, Qnew, **kwargs)
 
-    # output preparation
-    # timeseries_output = np.zeros(
-    #     (self.output_timesteps.shape[0], Q.shape[0], Q.shape[1])
-    # )
-    # timeseries_Q = self.save_output(time, Q, timeseries_Q)
     i_snapshot = 0
     dt_snapshot = time/(settings.output_snapshots-1)
     io.init_output_directory(settings.output_dir, settings.output_clean_dir)
     i_snapshot = io.save_fields(settings.output_dir, time, 0, i_snapshot, Qnew, Qaux, settings.output_write_all)
     mesh.write_to_hdf5(settings.output_dir)
 
-    # timeseries_output = []
-    # timeseries_output.append(( Q, Qaux, parameters, time ))
-
     time_start = gettime()
-
-    # Qavg_5steps = [Q, Q, Q, Q, Q]
-    # error = np.inf
-
-    # convert element_edge_normals (dict of elements keys -> list of edge normals) to array
-    # in case there are less than 4 (quad), 3(tri) normals in list due to bounary elements, fill array with existing normal
-    # normals = np.zeros((self.mesh.num_nodes_per_element, 3, Qnew.shape[1]))
-    # for k, v in self.mesh.element_edge_normal.items():
-    #     for i in range(len(v)):
-    #         normals[i, :, k] = v[i]
-    #     # fill remaining with copy of last normal (since I do not want to have zero eigenvalues, rather copies of existing ones)
-    #     for i in range(len(v), self.mesh.num_nodes_per_element):
-    #         normals[i, :, k] = v[-1]
 
     model_functions = model.get_runtime_model()
     _ = model.create_c_interface()
@@ -273,28 +193,15 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
     # map_elements_to_edges = recon.create_map_elements_to_edges(mesh)
     # on_edges_normal, on_edges_length = recon.get_edge_geometry_data(mesh)
     # space_solution_operator = get_semidiscrete_solution_operator(mesh, runtime_model, settings, on_edges_normal, on_edges_length, map_elements_to_edges)
-    space_solution_operator = get_semidiscrete_solution_operator_new(mesh, runtime_model, model.boundary_conditions, settings)
+    space_solution_operator = get_semidiscrete_solution_operator(mesh, runtime_model, model.boundary_conditions, settings)
     compute_max_abs_eigenvalue = _get_compute_max_abs_eigenvalue(mesh, runtime_model, model.boundary_conditions, settings)
     min_inradius = np.min(mesh.element_inradius)
 
 
-
     while (time < settings.time_end):
         Q = deepcopy(Qnew)
-        # Time step estimation
-        #TODO Hack
-        ev_abs_max = 0.
-        min_incircle = 0.
         dt = settings.compute_dt(Q, Qaux, parameters, min_inradius, compute_max_abs_eigenvalue)
 
-        # Repeat timestep for imaginary numbers
-        # EVs, imaginary = self.solver.compute_eigenvalues(Qnew, normals)
-
-        # TODO use this to avoid EV computation in flux. Problem
-        # finding the appropriate neigbor edge it to find EVj!
-        # kwargs["aux_fields"].update({"EVs": EVs})
-        # EVmax = np.abs(kwargs["aux_fields"]["EVs"]).max()
-        # dt = self.solver.compute_timestep_size(Qnew, self.cfl, EVmax)
         assert not np.isnan(dt) and np.isfinite(dt)
 
         # if iteration < self.warmup_interations:
@@ -306,11 +213,8 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
         if settings.truncate_last_time_step:
             if time + dt * 1.001 > settings.time_end:
                 dt = settings.time_end - time + 10 ** (-10)
-        # kwargs.update({"time": time, "dt": dt, "Qold": Q})
-        # Qnew, _ = self.solver.step(Qnew, **kwargs)
 
         Qnew = time_ode_solver(space_solution_operator, Q, Qaux, parameters, dt)
-        # model.boundary_conditions.apply(Qnew)
 
         # Qavg = np.mean(Qavg_5steps, axis=0)
         # error = (
@@ -324,15 +228,11 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
         time += dt
         iteration += 1
 
-        # kwargs.update({"time": time, "iteration": iteration})
         # for callback in self.callback_function_list_post_solvestep:
         #     Qnew, kwargs = callback(self, Qnew, **kwargs)
 
         i_snapshot = io.save_fields(settings.output_dir, time, (i_snapshot+1)*dt_snapshot, i_snapshot, Qnew, Qaux, settings.output_write_all)
         
-        # timeseries_Q = self.save_output(time, Qnew, timeseries_Q)
-        # timeseries_output.append(( Qnew, Qaux, parameters, dt ))
-
         # logger.info(
         #     "Iteration: {:6.0f}, Runtime: {:6.2f}, Time: {:2.4f}, dt: {:2.4f}, error: {}".format(
         #         iteration, gettime() - time_start, time, dt, error
@@ -341,6 +241,5 @@ def fvm_unsteady_semidiscrete(mesh, model, settings, time_ode_solver):
         progressbar.set_stat(min(time, settings.time_end))
         progressbar.update()
 
-    # self.vtk_write_timestamp_file()
     progressbar.end()
     return settings
