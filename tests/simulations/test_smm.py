0e4fe9cb1b0695d5d61868555fbc363462ac7673
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2024-01-03 11:08:52 +0100

    tc: 3d + channel

--- tests/simulations/test_smm.py
+++ tests/simulations/test_smm.py
@@ -8,6 +8,8 @@ import library.model.initial_conditions as IC
 import library.model.boundary_conditions as BC
 from library.pysolver.ode import RK1
 import library.misc.io as io
+from library.pysolver.reconstruction import GradientMesh
+import library.postprocessing.postprocessing as postprocessing
 
 @pytest.mark.critical
 @pytest.mark.unfinished
@@ -150,33 +152,22 @@ def test_steffler():
 @pytest.mark.unfinished
 def test_channel_with_hole_2d():
     level = 2
-    settings = Settings(name = "ShallowMoments2d", momentum_eqns = [1, 2] + [3+l  for l in range(2*level)], parameters = {'g':1., 'C': 1., 'nu': 0.0016}, reconstruction = recon.constant, num_flux = flux.LLF(), compute_dt = timestepping.adaptive(CFL=0.45), time_end = 30., output_snapshots = 100)
+    settings = Settings(name = "ShallowMoments2d", momentum_eqns = [1, 2] + [3+l  for l in range(2*level)], parameters = {'g':9.81, 'C': 16., 'nu': 0.0016}, reconstruction = recon.constant, num_flux = flux.LLF(), compute_dt = timestepping.adaptive(CFL=0.45), time_end = 5., output_snapshots = 100)
 
 
-    inflow_dict = {i: 0. for i in range(1, 2*(1+level)+1)}
-    inflow_dict[1] = 0.36
-    outflow_dict = {0: 1.0}
+    # inflow_dict = {i: 0. for i in range(1, 2*(1+level)+1)}
+    # inflow_dict[1] = 0.36
+    # outflow_dict = {0: 1.0}
+
+    inflow_dict = {i: 0. for i in range(0, 2*(1+level)+1)}
+    inflow_dict[0] = 0.1
+    inflow_dict[1] = -0.2
+    outflow_dict = {}
 
     bcs = BC.BoundaryConditions(
-        [BC.Wall(physical_tag='hole'), BC.Wall(physical_tag='left'),BC.Wall(physical_tag='right'), BC.InflowOutflow(physical_tag='bottom', prescribe_fields=inflow_dict), BC.InflowOutflow(physical_tag='top', prescribe_fields=outflow_dict)]
+        [BC.Wall(physical_tag='hole'), BC.Extrapolation(physical_tag='top'),BC.Extrapolation(physical_tag='bottom'), BC.InflowOutflow(physical_tag='left', prescribe_fields=inflow_dict), BC.InflowOutflow(physical_tag='right', prescribe_fields=outflow_dict)]
     )
-    ic = IC.Constant(constants=lambda n_fields:np.array([1.0, 0.0] + [0. for i in range(n_fields-2)]))
-    model = ShallowMoments2d(
-        dimension=2,
-        fields=3+2*level,
-        aux_fields=0,
-        parameters=settings.parameters,
-        boundary_conditions=bcs,
-        initial_conditions=ic,
-        settings={'friction': []},
-    )
-    main_dir = os.getenv("SMS")
-    mesh = Mesh.load_gmsh(
-        os.path.join(main_dir, "meshes/channel_2d_hole/mesh_coarse.msh"),
-        'triangle'
-    )
-
-
+    ic = IC.Constant(constants=lambda n_fields:np.array([1.0, 0.1] + [0. for i in range(n_fields-2)]))
     model = ShallowMoments2d(
         dimension=2,
         fields=3+2*level,
@@ -186,10 +177,51 @@ def test_channel_with_hole_2d():
         initial_conditions=ic,
         settings={'friction': ['chezy']},
     )
-
+    main_dir = os.getenv("SMS")
+    mesh = Mesh.load_gmsh(
+        os.path.join(main_dir, "meshes/channel_2d_hole/mesh_mid.msh"),
+        'triangle'
+    )
 
     fvm_unsteady_semidiscrete(mesh, model, settings, RK1)
     io.generate_vtk(settings.output_dir)
+
+@pytest.mark.critical
+@pytest.mark.unfinished
+def test_smm_grad_2d():
+    mesh_type='triangle'
+    level = 2
+    settings = Settings(name = "ShallowMoments2d", momentum_eqns = [1, 2] + [3+l  for l in range(2*level)], parameters = {'g':1.0, 'C': 16., 'nu': 0.1}, reconstruction = recon.constant, num_flux = flux.LLF(), compute_dt = timestepping.adaptive(CFL=0.45), time_end = 1., output_snapshots = 30)
+
+
+    bc_tags = ["left", "right", "top", "bottom"]
+    bc_tags_periodic_to = ["right", "left", "bottom", "top"]
+
+    bcs = BC.BoundaryConditions(
+        [BC.Periodic(physical_tag=tag, periodic_to_physical_tag=tag_periodic_to) for (tag, tag_periodic_to) in zip(bc_tags, bc_tags_periodic_to)]
+    )
+    ic = IC.RP(left=lambda n_field: np.array([2., 0., 0.] + [0. for l in range(2*level)]), right=lambda n_field: np.array([1., 0., 0.]+ [0. for l in range(2*level)]) )
+    model = ShallowMoments2d(
+        dimension=2,
+        fields=3+2*level,
+        aux_fields=0,
+        parameters=settings.parameters,
+        boundary_conditions=bcs,
+        initial_conditions=ic,
+        settings={'friction': ['chezy']},
+    )
+    main_dir = os.getenv("SMS")
+    mesh = Mesh.load_gmsh(
+        os.path.join(main_dir, "meshes/{}_2d/mesh_coarse.msh".format(mesh_type)),
+        mesh_type
+    )
+    mesh = GradientMesh.fromMesh(mesh)
+
+
+    fvm_unsteady_semidiscrete(mesh, model, settings, RK1)
+    # io.generate_vtk(settings.output_dir)
+    postprocessing.recover_3d_from_smm_as_vtk(model, settings.output_dir, os.path.join(settings.output_dir, 'mesh.hdf5'), os.path.join(settings.output_dir, 'fields.hdf5'),Nz=10, start_at_time=1. )
+    
 
 
 if __name__ == "__main__":
@@ -198,4 +230,5 @@ if __name__ == "__main__":
     # test_smm_2d("triangle")
     # test_inflowoutflow_2d()
     # test_steffler()
-    test_channel_with_hole_2d()
+    # test_channel_with_hole_2d()
+    test_smm_grad_2d()
