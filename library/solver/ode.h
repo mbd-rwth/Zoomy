10237f5ccd7f6345b4a3174f01df4f4d092fa074
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2024-01-30 11:32:26 +0100

    BUG: make command line option for n_elements

--- library/solver/ode.h
+++ library/solver/ode.h
@@ -1,17 +1,42 @@
 #include "define.h"
+#include "model.h"
+#include "mesh.h"
+#include "space_solution_operator.h"
 
-typedef void (*F)(const realArr2, const realArr2, const realArr, realArr2& out);
-typedef void (*dFdQ)(const realArr2, const realArr2, const realArr, realArr3& out);
+#ifndef ODE_H
+#define ODE_H
 
-void RK1(F func, const realArr2 Q, const realArr2 Qaux, const realArr param, double dt, realArr2& out, dFdQ func_jac=nullptr)
+void RK1(OdeOperator& func, const realArr2 Q, const realArr2 Qaux, const realArr param, double dt, realArr2& out)
 {
-    realArr2 dQ("dQ", Q.extent(0), Q.extent(1));
-    func(Q, Qaux, param, dQ);
-    for (int i = 0; i < Q.extent(0); ++i)
+    const int n_elements = Q.extent(0);
+    const int n_fields = Q.extent(1);
+    realArr2 dQ("dQ", n_elements, n_fields);
+    func.evaluate(Q, Qaux, param, dQ);
+    for (int i = 0; i < n_elements; ++i)
     {
-        for (int j = 0; j < Q.extent(1); ++j)
+        for (int j = 0; j < n_fields; ++j)
         {
             out(i, j) = Q(i, j) + dt * dQ(i, j);
         }
     }
 }
+
+
+class Integrator
+{
+private:
+    void (*method)(OdeOperator&, const realArr2, const realArr2, const realArr, double, realArr2&);
+public:
+    Integrator(std::string method) {
+        if (method == "RK1")
+        {
+            this->method = RK1;
+        }
+    }
+    void evaluate(OdeOperator& func, const realArr2 Q, const realArr2 Qaux, const realArr param, double dt, realArr2& out)
+    {
+        this->method(func, Q, Qaux, param, dt, out);
+    }
+};
+
+#endif // ODE_H
