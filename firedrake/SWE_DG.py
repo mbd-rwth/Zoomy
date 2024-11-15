d57bff04ba76e508e86b5b8940705ad1aa9a603b
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2024-11-15 16:14:36 +0100

    WIP firedrake DG

--- firedrake/SWE_DG.py
+++ firedrake/SWE_DG.py
@@ -7,8 +7,8 @@ import math
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-N = 100
-mesh = UnitSquareMesh(N, N, quadrilateral=True)
+N = 40
+mesh = UnitSquareMesh(N, N, quadrilateral=False)
 
 V = FunctionSpace(mesh, "DG", 0)
 Vout = FunctionSpace(mesh, "DG", 0)
@@ -31,8 +31,10 @@ height = conditional(Or(And(And(x < x0+width, x > x0-width), And(y < y0+width, y
 velocity = conditional(Or(And(And(x < x0+width, x > x0-width), And(y < y0+width, y > y0-width) ), And(And(x < x1+width, x > x1-width), And(y < y1+width, y > y1-width) )), as_vector((0.0, 0.0)) , as_vector((0., 0.)))
 
 IC = Function(VW, name="IC")
-IC.sub(0).assign(Function(VW.sub(0)).interpolate(height))
-IC.sub(1).assign(Function(VW.sub(1)).interpolate(height * velocity))
+h0 = Function(VW.sub(0)).interpolate(height)
+hu0 = Function(VW.sub(1)).interpolate(height * velocity)
+IC.sub(0).assign(h0)
+IC.sub(1).assign(hu0)
 
 Q = Function(VW, name="Q")
 Q_ = Function(VW, name="Qold")
@@ -47,7 +49,7 @@ v_, w_ = TrialFunctions(VW)
 a = inner(w, w_) * dx + inner(v, v_) * dx
 
 T = 0.01
-CFL = 0.45
+CFL = 0.1
 incircle = (1./N)
 g = 9.81
 
@@ -56,45 +58,31 @@ n = FacetNormal(mesh)
 I = Identity(2)
 P = lambda h, hu: sqrt(0.5 * g * h * h) * I
 ev = lambda h, hu, n: abs(dot(hu, n)/h) + sqrt(g * h)
+f_h = lambda h, hu : hu
+f_hu = lambda h, hu: outer(hu, hu) / h + P(h, hu)
 
 h_D = h
 hu_D = hu - 2 * dot(hu, n) * n
 hun_D = dot(hu_D, n)
 
-flux_h = lambda h, hu, n : dot(hu, n)
-flux_hu = lambda h, hu, n: dot(outer(hu, hu) / h + P(h, hu), n)
 
 p = '+'
 m = '-'
-F_H_l = (hu(p))
-F_H_r = (hu(m))
-F_H_n = 0.5*dot((F_H_l + F_H_r), n(m))
-F_dis_H = -0.5  * avg(ev(h, hu, n)) * (h(p)-h(m))
-F_H = (v(p)-v(m)) * (F_H_n + F_dis_H) * dS
-
-F_HU_l = outer(hu(p), hu(p)) / h(p) + 0.5 * g * h(p) * h(p) * I
-F_HU_r = outer(hu(m), hu(m)) / h(m) + 0.5 * g * h(m) * h(m) * I
-F_HU_n = 0.5*dot((F_HU_l + F_HU_r), n(m))
-F_dis_HU = -0.5 * avg(ev(h, hu, n)) * (hu(p)-hu(m))
-F_HU = dot((w(p)-w(m)), (F_HU_n + F_dis_HU)) * dS
 
 h_g = h
 hu_n = dot(hu, n) * n
 hu_t = hu - hu_n
 hu_g = -hu_n + hu_t
+n_g = -n
 
-BC_H_l = hu
-BC_H_r = hu_g
-BC_H_n = 0.5*dot((BC_H_l + BC_H_r), n)
-BC_dis_H = -0.5 * ev(h, hu, n) * (h_g - h)
-BC_H = -v*(BC_H_n + BC_dis_H) * ds
+DG_H = inner(grad(v), f_h(h, hu)) * ds
+DG_HU = inner(grad(w), f_hu(h, hu)) * ds
 
-BC_HU_l = outer(hu, hu) / h + 0.5 * g * h * h * I
-BC_HU_r = outer(hu_g, hu_g) / h_g + 0.5 * g * h_g * h_g * I
-BC_HU_n = 0.5*dot((BC_HU_l + BC_HU_r), n)
-BC_dis_HU = -0.5 * ev(h, hu, n) * (hu_g - hu)
-BC_HU = -dot(w, (BC_HU_n + BC_dis_HU)) * ds
+F_H = (v(p)-v(m)) * (dot(0.5*(f_h(h(p), hu(p)) + f_h(h(m), hu(m))), n(m)) - 0.5*avg(ev(h, hu, n))*(h(p) - h(m))) * dS
+F_HU = dot((w(p)-w(m)), (dot(0.5*(f_hu(h(p), hu(p)) + f_hu(h(m), hu(m))), n(m)) - 0.5*avg(ev(h, hu, n))*(hu(p) - hu(m)))) * dS
 
+BC_H = -v * (dot(0.5*(f_h(h, hu) + f_h(h_g, hu_g)), n) - 0.5*0.5*(ev(h, hu, n)+ ev(h_g, hu_g, n_g))*(h_g - h)) * ds
+BC_HU = -dot(w, dot(0.5*(f_hu(h, hu) + f_hu(h_g, hu_g)), n) - 0.5*0.5*(ev(h, hu, n)+ ev(h_g, hu_g, n_g))*(hu_g - hu)) * ds
 
 t = 0.0
 print("t=", t)
@@ -102,27 +90,55 @@ step = 0
 output_freq = 1
 
 outfile = VTKFile("swe.pvd")
-outfile.write(project(h, V, name="H"), project(hu, W, name="HU"), time=t)
-
-h0 = deepcopy(h)
+outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
 
 
 ev_cell = lambda h, hu: abs(hu[0]) / h + sqrt(g * h)
 ev_max = max(project(ev_cell(h, hu), V).vector())
 dt = CFL * incircle / ev_max
-L = dt * (F_H + F_HU + BC_H + BC_HU)
+L = dt * (DG_H + DG_HU + F_H + F_HU + BC_H + BC_HU)
+
+Q1 = Function(VW)
+Q2 = Function(VW)
+h1, hu1 = split(Q1)
+h2, hu2 = split(Q2)
+L1 = replace(L, {h: h1, hu: hu1}); 
+L2 = replace(L, {h: h2, hu: hu2}); 
 
 params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}
 dQ = Function(VW)
 prob = LinearVariationalProblem(a, L, dQ)
 solv = LinearVariationalSolver(prob, solver_parameters=params)
+prob1 = LinearVariationalProblem(a, L1, dQ)
+solv1 = LinearVariationalSolver(prob1, solver_parameters=params)
+prob2 = LinearVariationalProblem(a, L2, dQ)
+solv2 = LinearVariationalSolver(prob2, solver_parameters=params)
 while t < T - 0.5 * dt:
     ev_max = max(project(ev_cell(h, hu), V).vector())
     dt = CFL * incircle / ev_max
+
     # solve(F == 0, Q)
+
+    #solv.solve()
+    #Q.assign(Q_ + dQ)
+    #Q.assign(Q)
+    #Q_.assign(Q)
+
     solv.solve()
-    Q.assign(Q + dQ)
-    Q_.assign(Q)
+    Q1.assign(Q_ + dQ)
+    solv1.solve()
+    Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
+    Q.assign(Q2)
+    Q_.assign(Q2)
+
+    #solv.solve()
+    #Q.assign(Q_ + dQ)
+    #solv1.solve()
+    #Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
+    #solv2.solve()
+    #Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
+    #Q.assign(Q2)
+    #Q_.assign(Q2)
 
     step += 1
     t += dt
@@ -130,5 +146,8 @@ while t < T - 0.5 * dt:
     if step % output_freq == 0:
         max_h = project(h, V).vector().max()
         error = assemble((abs(h-h0))*dx)
-        outfile.write(project(h, V, name="H"), project(hu, W, name="HU"), time=t)
-        print(f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L1={error}, h_max={max_h}")
+        L2_err = sqrt(assemble((h - h0)*(h - h0)*dx))
+        L2_init = sqrt(assemble(h0*h0*dx))
+        error = L2_err / L2_init
+        outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
+        print(f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}")
