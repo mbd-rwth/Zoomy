667d4eb5f10e29ac3c1ce599a1b121f49be82ad1
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2024-11-21 07:47:21 +0100

    FD extrusion mesh

--- firedrake/SWE_DG.py
+++ firedrake/SWE_DG.py
@@ -7,12 +7,14 @@ import math
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-N = 100
+N = 40
 mesh = UnitSquareMesh(N, N, quadrilateral=True)
 
+Vl = FunctionSpace(mesh, "DG", 1)
 V = FunctionSpace(mesh, "DG", 1)
 Vout = FunctionSpace(mesh, "DG", 1)
 W = VectorFunctionSpace(mesh, "DG", 1)
+Wl = VectorFunctionSpace(mesh, "DG", 1)
 Wout = VectorFunctionSpace(mesh, "DG", 1)
 
 VW = V * W
@@ -27,10 +29,26 @@ x1 = 0.7
 y1 = 0.5
 
 width = 0.05
-height = conditional(Or(And(And(x < x0+width, x > x0-width), And(y < y0+width, y > y0-width) ), And(And(x < x1+width, x > x1-width), And(y < y1+width, y > y1-width) )), 8.0, 5.0)
-velocity = conditional(Or(And(And(x < x0+width, x > x0-width), And(y < y0+width, y > y0-width) ), And(And(x < x1+width, x > x1-width), And(y < y1+width, y > y1-width) )), as_vector((0.0, 0.0)) , as_vector((0., 0.)))
+height = conditional(
+    Or(
+        And(And(x < x0 + width, x > x0 - width), And(y < y0 + width, y > y0 - width)),
+        And(And(x < x1 + width, x > x1 - width), And(y < y1 + width, y > y1 - width)),
+    ),
+    8.0,
+    5.0,
+)
+velocity = conditional(
+    Or(
+        And(And(x < x0 + width, x > x0 - width), And(y < y0 + width, y > y0 - width)),
+        And(And(x < x1 + width, x > x1 - width), And(y < y1 + width, y > y1 - width)),
+    ),
+    as_vector((0.0, 0.0)),
+    as_vector((0.0, 0.0)),
+)
 
 IC = Function(VW, name="IC")
+# ht = Function(Vl).interpolate(height)
+# ht.assign(ht)
 h0 = Function(VW.sub(0)).interpolate(height)
 hu0 = Function(VW.sub(1)).interpolate(height * velocity)
 IC.sub(0).assign(h0)
@@ -41,6 +59,7 @@ Q_ = Function(VW, name="Qold")
 Q.assign(IC)
 Q_.assign(IC)
 
+
 h, hu = split(Q)
 h_, hu_ = split(Q_)
 
@@ -49,16 +68,16 @@ v_, w_ = TrialFunctions(VW)
 a = inner(w, w_) * dx + inner(v, v_) * dx
 
 T = 3.0
-CFL = 1. / (2 + 2 + 1)
-incircle = (1./N)
+CFL = 1.0 / (2 + 2 + 1)
+incircle = 1.0 / N
 g = 9.81
 
 n = FacetNormal(mesh)
 
 I = Identity(2)
 P = lambda h, hu: sqrt(0.5 * g * h * h) * I
-ev = lambda h, hu, n: abs(dot(hu, n)/h) + sqrt(g * h)
-f_h = lambda h, hu : hu
+ev = lambda h, hu, n: abs(dot(hu, n) / h) + sqrt(g * h)
+f_h = lambda h, hu: hu
 f_hu = lambda h, hu: outer(hu, hu) / h + P(h, hu)
 
 h_D = h
@@ -66,8 +85,8 @@ hu_D = hu - 2 * dot(hu, n) * n
 hun_D = dot(hu_D, n)
 
 
-p = '+'
-m = '-'
+p = "+"
+m = "-"
 
 h_g = h
 hu_n = dot(hu, n) * n
@@ -78,11 +97,41 @@ n_g = -n
 DG_H = inner(grad(v), f_h(h, hu)) * dx
 DG_HU = inner(grad(w), f_hu(h, hu)) * dx
 
-F_H = (v(p)-v(m)) * (dot(0.5*(f_h(h(p), hu(p)) + f_h(h(m), hu(m))), n(m)) - 0.5*avg(ev(h, hu, n))*(h(p) - h(m))) * dS
-F_HU = dot((w(p)-w(m)), (dot(0.5*(f_hu(h(p), hu(p)) + f_hu(h(m), hu(m))), n(m)) - 0.5*avg(ev(h, hu, n))*(hu(p) - hu(m)))) * dS
+F_H = (
+    (v(p) - v(m))
+    * (
+        dot(0.5 * (f_h(h(p), hu(p)) + f_h(h(m), hu(m))), n(m))
+        - 0.5 * avg(ev(h, hu, n)) * (h(p) - h(m))
+    )
+    * dS
+)
+F_HU = (
+    dot(
+        (w(p) - w(m)),
+        (
+            dot(0.5 * (f_hu(h(p), hu(p)) + f_hu(h(m), hu(m))), n(m))
+            - 0.5 * avg(ev(h, hu, n)) * (hu(p) - hu(m))
+        ),
+    )
+    * dS
+)
 
-BC_H = -v * (dot(0.5*(f_h(h, hu) + f_h(h_g, hu_g)), n) - 0.5*0.5*(ev(h, hu, n)+ ev(h_g, hu_g, n_g))*(h_g - h)) * ds
-BC_HU = -dot(w, dot(0.5*(f_hu(h, hu) + f_hu(h_g, hu_g)), n) - 0.5*0.5*(ev(h, hu, n)+ ev(h_g, hu_g, n_g))*(hu_g - hu)) * ds
+BC_H = (
+    -v
+    * (
+        dot(0.5 * (f_h(h, hu) + f_h(h_g, hu_g)), n)
+        - 0.5 * 0.5 * (ev(h, hu, n) + ev(h_g, hu_g, n_g)) * (h_g - h)
+    )
+    * ds
+)
+BC_HU = (
+    -dot(
+        w,
+        dot(0.5 * (f_hu(h, hu) + f_hu(h_g, hu_g)), n)
+        - 0.5 * 0.5 * (ev(h, hu, n) + ev(h_g, hu_g, n_g)) * (hu_g - hu),
+    )
+    * ds
+)
 
 t = 0.0
 print("t=", t)
@@ -102,11 +151,10 @@ Q1 = Function(VW)
 Q2 = Function(VW)
 h1, hu1 = split(Q1)
 h2, hu2 = split(Q2)
-L1 = replace(L, {h: h1, hu: hu1}); 
-L2 = replace(L, {h: h2, hu: hu2}); 
-
-#params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}
-params = {'ksp_type': 'cg'}
+L1 = replace(L, {h: h1, hu: hu1})
+L2 = replace(L, {h: h2, hu: hu2})
+# params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}
+params = {"ksp_type": "cg"}
 dQ = Function(VW)
 # prob = LinearVariationalProblem(a, L, dQ)
 prob = LinearVariationalProblem(a, L, dQ)
@@ -116,15 +164,35 @@ solv1 = LinearVariationalSolver(prob1, solver_parameters=params)
 prob2 = LinearVariationalProblem(a, L2, dQ)
 solv2 = LinearVariationalSolver(prob2, solver_parameters=params)
 limiterH = VertexBasedLimiter(V)
-#limiterH.compute_bounds(Q.sub(1))
-#limiterH.apply(Q.sub(0))
-#limiterH.apply(Q_.sub(0))
-#limiterH.apply_limiter(Q.sub(0))
-#limiterH.apply_limiter(Q_.sub(0))
-#print(np.max(Q.sub(0).dat.data_ro))
-#print(np.min(Q.sub(0).dat.data_ro))
-#print('---------------')
+# limiterH.compute_bounds(Q.sub(1))
+# limiterH.apply(Q.sub(0))
+# limiterH.apply(Q_.sub(0))
+# limiterH.apply_limiter(Q.sub(0))
+# limiterH.apply_limiter(Q_.sub(0))
+# print(np.max(Q.sub(0).dat.data_ro))
+# print(np.min(Q.sub(0).dat.data_ro))
+# print('---------------')
 tmp_func = V.get_work_function()
+
+
+T = 3 * dt
+
+
+def apply_limiter_P1(field):
+    field_P1 = project(field, Vl)
+    limiter.apply(field_P1)
+    field = project
+    return field_P1
+
+
+def apply_limiter(Q):
+    q_limit = project(Q.sub(0), Vl)
+    limiterH.apply(Q.sub(0))
+    for i in range(W.value_size):
+        tmp_func.dat.data_with_halos[:] = Q.sub(1).dat.data_with_halos[:, i]
+        limiterH.apply(tmp_func)
+        Q.sub(1).dat.data_with_halos[:, i] = tmp_func.dat.data_with_halos
+
 
 while t < T - 0.5 * dt:
     ev_max = max(project(ev_cell(h, hu), V).vector())
@@ -132,58 +200,197 @@ while t < T - 0.5 * dt:
 
     # solve(F == 0, Q)
 
-    #limiterH.compute_bounds(Q.sub(0))
-    solv.solve()
-    Q.assign(Q_ + dQ)
-    #limiterH.apply_limiter(Q.sub(0))
-    #limiterH = VertexBasedLimiter(V)
-    limiterH.apply(Q.sub(0))
-    #print(W.value_size)
-    for i in range(W.value_size):
-        d = Q.sub(1).dat.data_with_halos
-        dd = tmp_func.dat.data_with_halos
-        #print(d.shape, dd.shape)
-        tmp_func.dat.data_with_halos[:] = d[:, i]
-        limiterH.apply(tmp_func)
-        Q.sub(1).dat.data_with_halos[:, i] = tmp_func.dat.data_with_halos
-
-
-    #limiterH.apply(Q.sub(1))
-    #print(np.max(Q.sub(0).dat.data_ro))
-    #print(np.min(Q.sub(0).dat.data_ro))
-    #limiterH.apply(dQ.sub(0))
-    #Q.assign(Q)
-    Q_.assign(Q)
-    #limiterH.apply(Q_.sub(0))
-    #limiterH.apply_limiter(Q_.sub(0))
-    #print(np.max(Q.sub(0).dat.data_ro))
-    #print(np.min(Q.sub(0).dat.data_ro))
-    #print('---------------')
-
-    # solv.solve()
-    # Q1.assign(Q_ + dQ)
-    # solv1.solve()
-    # Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
-    # Q.assign(Q2)
-    # Q_.assign(Q2)
-
+    # limiterH.compute_bounds(Q.sub(0))
     #solv.solve()
     #Q.assign(Q_ + dQ)
-    #solv1.solve()
-    #Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
-    #solv2.solve()
-    #Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
-    #Q.assign(Q2)
-    #Q_.assign(Q2)
+    #apply_limiter(Q)
+    #Q_.assign(Q)
+
+    # h, hu = split(Q)
+    # h_, hu_ = split(Q_)
+
+    # print(f'h: {Q.sub(0).dat.data_with_halos.shape}')
+    # ht.project(h)
+    # print(f'ht: {ht.dat.data_with_halos.shape}')
+    # limiterH.apply_limiter(Q.sub(0))
+    # limiterH = VertexBasedLimiter(V)
+    # print(W.value_size)
+
+    # limiterH.apply(Q.sub(1))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # limiterH.apply(dQ.sub(0))
+    # Q.assign(Q)
+    # limiterH.apply(Q_.sub(0))
+    # limiterH.apply_limiter(Q_.sub(0))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # print('---------------')
+
+    solv.solve()
+    Q1.assign(Q_ + dQ)
+    apply_limiter(Q1)
+    solv1.solve()
+    Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
+    apply_limiter(Q2)
+    Q.assign(Q2)
+    Q_.assign(Q2)
+
+    # solv.solve()
+    # Q.assign(Q_ + dQ)
+    # solv1.solve()
+    # Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
+    # solv2.solve()
+    # Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
 
     step += 1
     t += dt
 
     if step % output_freq == 0:
         max_h = project(h, V).vector().max()
-        error = assemble((abs(h-h0))*dx)
-        L2_err = sqrt(assemble((h - h0)*(h - h0)*dx))
-        L2_init = sqrt(assemble(h0*h0*dx))
+        error = assemble((abs(h - h0)) * dx)
+        L2_err = sqrt(assemble((h - h0) * (h - h0) * dx))
+        L2_init = sqrt(assemble(h0 * h0 * dx))
         error = L2_err / L2_init
         outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
-        print(f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}")
+        print(
+            f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}"
+        )
+    # limiterH.apply_limiter(Q.sub(0))
+    # limiterH = VertexBasedLimiter(V)
+    # print(W.value_size)
+
+    # limiterH.apply(Q.sub(1))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # limiterH.apply(dQ.sub(0))
+    # Q.assign(Q)
+    # limiterH.apply(Q_.sub(0))
+    # limiterH.apply_limiter(Q_.sub(0))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # print('---------------')
+
+    # solv.solve()
+    # Q1.assign(Q_ + dQ)
+    # apply_limiter(Q1)
+    # solv1.solve()
+    # Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
+    # apply_limiter(Q2)
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    # solv.solve()
+    # Q.assign(Q_ + dQ)
+    # solv1.solve()
+    # Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
+    # solv2.solve()
+    # Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    step += 1
+    t += dt
+
+    if step % output_freq == 0:
+        max_h = project(h, V).vector().max()
+        error = assemble((abs(h - h0)) * dx)
+        L2_err = sqrt(assemble((h - h0) * (h - h0) * dx))
+        L2_init = sqrt(assemble(h0 * h0 * dx))
+        error = L2_err / L2_init
+        outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
+        print(
+            f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}"
+        )
+    # limiter = VertexBasedLimiter(V)
+    # print(W.value_size)
+
+    # limiter.apply(Q.sub(1))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # limiter.apply(dQ.sub(0))
+    # Q.assign(Q)
+    # limiter.apply(Q_.sub(0))
+    # limiter.apply_limiter(Q_.sub(0))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # print('---------------')
+
+    # solv.solve()
+    # Q1.assign(Q_ + dQ)
+    # apply_limiter(Q1)
+    # solv1.solve()
+    # Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
+    # apply_limiter(Q2)
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    # solv.solve()
+    # Q.assign(Q_ + dQ)
+    # solv1.solve()
+    # Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
+    # solv2.solve()
+    # Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    step += 1
+    t += dt
+
+    if step % output_freq == 0:
+        max_h = project(h, V).vector().max()
+        error = assemble((abs(h - h0)) * dx)
+        L2_err = sqrt(assemble((h - h0) * (h - h0) * dx))
+        L2_init = sqrt(assemble(h0 * h0 * dx))
+        error = L2_err / L2_init
+        outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
+        print(
+            f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}"
+        )
+    # limiter = VertexBasedLimiter(V)
+    # print(W.value_size)
+
+    # limiter.apply(Q.sub(1))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # limiter.apply(dQ.sub(0))
+    # Q.assign(Q)
+    # limiter.apply(Q_.sub(0))
+    # limiter.apply_limiter(Q_.sub(0))
+    # print(np.max(Q.sub(0).dat.data_ro))
+    # print(np.min(Q.sub(0).dat.data_ro))
+    # print('---------------')
+
+    # solv.solve()
+    # Q1.assign(Q_ + dQ)
+    # apply_limiter(Q1)
+    # solv1.solve()
+    # Q2.assign(0.5 * Q_ + 0.5 * (Q1+ dQ))
+    # apply_limiter(Q2)
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    # solv.solve()
+    # Q.assign(Q_ + dQ)
+    # solv1.solve()
+    # Q1.assign(0.75 * Q_ + 0.25 * (Q+ dQ))
+    # solv2.solve()
+    # Q2.assign(1./3. * Q_ + 2./3. * (Q1+ dQ))
+    # Q.assign(Q2)
+    # Q_.assign(Q2)
+
+    step += 1
+    t += dt
+
+    if step % output_freq == 0:
+        max_h = project(h, V).vector().max()
+        error = assemble((abs(h - h0)) * dx)
+        L2_err = sqrt(assemble((h - h0) * (h - h0) * dx))
+        L2_init = sqrt(assemble(h0 * h0 * dx))
+        error = L2_err / L2_init
+        outfile.write(project(h, Vout, name="H"), project(hu, Wout, name="HU"), time=t)
+        print(
+            f"t={t}, dt={dt}, ev_max={ev_max}, |h-h_0|_L2/|h_0|_L2={error}, h_max={max_h}"
+        )
