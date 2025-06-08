133579287cc68fea9260ed89f569e954eaa1745e
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2025-06-08 18:27:13 +0200

    coupling case

---
+++ of_coupling/of-original/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+blockMesh
+cp -r 0.orig 0
+
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+
+close_log
