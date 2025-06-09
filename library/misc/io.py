cde416f6febac8ac928a368903a6bf01896aee30
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2025-06-09 13:23:52 +0200

    Cherry-Pick from AD Working: 9061dee514aa6980bf3300d2b5cb848db3aa7b06

--- library/misc/io.py
+++ library/misc/io.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import jax
 import jax.numpy as jnp
 import h5py
 import meshio
@@ -61,33 +62,21 @@ def clean_files(filepath, filename=".vtk"):
                 os.remove(os.path.join(abs_filepath, file))
 
 
-def save_fields(
-    filepath,
-    time,
-    next_write_at,
-    i_snapshot,
-    Q,
-    Qaux,
-    write_all,
-):
-    if not write_all and time < next_write_at:
-        return i_snapshot
-
-    _save_fields_to_hdf5(filepath, i_snapshot, time, Q, Qaux)
-    return i_snapshot + 1
 
 
 def _save_fields_to_hdf5(
     filepath, i_snapshot, time, Q, Qaux=None, overwrite=True
 ):
+    i_snap = int(i_snapshot)
+    jax.debug.print('SAVING')
     main_dir = os.getenv("SMS")
     filepath = os.path.join(main_dir, filepath)
     with h5py.File(filepath, "a") as f:
-        if i_snapshot == 0 and not 'fields' in f.keys():
+        if i_snap == 0 and not 'fields' in f.keys():
             fields = f.create_group('fields')
         else:
             fields = f['fields']
-        group_name = "iteration_" + str(i_snapshot)
+        group_name = "iteration_" + str(i_snap)
         if group_name in fields:
             if overwrite:
                 del fields[group_name]
@@ -98,6 +87,69 @@ def _save_fields_to_hdf5(
         attrs.create_dataset("Q", data=Q)
         if Qaux is not None:
             attrs.create_dataset("Qaux", data=Qaux)
+    return i_snapshot + 1.
+
+
+def get_save_fields(_filepath, write_all):
+    def _save_hdf5(
+    i_snapshot, time, Q, Qaux
+    ):
+        i_snap = int(i_snapshot)
+        main_dir = os.getenv("SMS")
+        filepath = os.path.join(main_dir, _filepath)
+
+        with h5py.File(filepath, "a") as f:
+            if i_snap == 0 and not 'fields' in f.keys():
+                fields = f.create_group('fields')
+            else:
+                fields = f['fields']
+            group_name = "iteration_" + str(i_snap)
+            if group_name in fields:
+                if overwrite:
+                    del fields[group_name]
+                else:
+                    raise ValueError(f"Group {group_name} already exists in {filename}")
+            attrs = fields.create_group(group_name)
+            attrs.create_dataset("time", data=time, dtype=float)
+            attrs.create_dataset("Q", data=Q)
+            if Qaux is not None:
+                attrs.create_dataset("Qaux", data=Qaux)
+        return i_snapshot + 1.
+    
+    def save_fields(time, next_write_at, i_snapshot, Q, Qaux):
+        condition = jnp.logical_and(not write_all, time < next_write_at)
+        def do_nothing(_):
+            return i_snapshot
+
+        def do_save(_):
+            # We define a small custom_jvp function that does the side effect
+            @jax.custom_jvp
+            def _save(i_snapshot, time, Q, Qaux):
+                return jax.pure_callback(_save_hdf5, jax.ShapeDtypeStruct((), jnp.float32), i_snapshot, time, Q, Qaux)
+                # return i_snapshot + 1.
+
+            @_save.defjvp
+            def _save_jvp(primals, tangents):
+                # Primal evaluation
+                primal_out = _save(*primals)
+                # Derivative is zero. We match shape/dtype (float32 scalar)
+                # so returning 0.0 is valid.
+                return primal_out, jnp.float32(0.0)
+
+            # Then return the new integer snapshot
+            return _save(i_snapshot, time, Q, Qaux)
+
+        return jax.lax.cond(condition, do_nothing, do_save, operand=None)
+
+    return save_fields
+
+def save_fields_test(a):
+    filepath, time, next_write_at, i_snapshot, Q, Qaux, write_all = a
+    if not write_all and time < next_write_at:
+        return i_snapshot
+
+    _save_fields_to_hdf5(filepath, i_snapshot, time, Q, Qaux)
+    return i_snapshot + 1
 
 
 def load_fields_from_hdf5(filepath, i_snapshot=-1):
