99cc8da9b74fa71fea28349872b30935c1c78e7a
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2025-07-21 17:00:04 +0200

    WIP: linear analysis with constraints

--- library/model/analysis.py
+++ library/model/analysis.py
@@ -1,9 +1,9 @@
 import os
-from sympy import symbols, Matrix, diff, exp, I, linear_eq_to_matrix, pprint, solve , Eq, Float, Rational
+from sympy import symbols, Matrix, diff, exp, I, linear_eq_to_matrix, pprint, solve , Eq, Float, Rational, zeros, simplify
 
 
 
-def linear_stability_analysis(model, substitutions, linear_system_variables):
+def linear_stability_analysis(model, substitutions, linear_system_variables, constraints=None):
     """
     Perform linear stability analysis on the given model with specified substitutions.
     
@@ -25,12 +25,19 @@ def linear_stability_analysis(model, substitutions, linear_system_variables):
     X = [x, y, z]
     A = model.sympy_quasilinear_matrix 
     S = model.sympy_source_implicit
+    if constraints is not None:
+        C = constraints
+    else:
+        C = zeros(0, 1)
         
     for k, v in substitutions.items():
         Q = Q.subs(k, v)
         for d in range(dim):
             A[d] = A[d].subs(k, v)
         S = S.subs(k, v)
+        C = C.subs(k, v)
+    
+    C = C.doit()
         
     
         
@@ -41,10 +48,9 @@ def linear_stability_analysis(model, substitutions, linear_system_variables):
         AgradQ += A[d] * gradQ[:, d]
 
 
-    expr = (diff(Q, t) + AgradQ - S) 
-    for i in range(len(expr)):
-        expr[i] = expr[i].replace(lambda expr: isinstance(expr, Float), lambda f: Rational(f))
-    
+    expr = Matrix.vstack((diff(Q, t) + AgradQ - S) , C)
+    # for i in range(len(expr)):
+    #     expr[i] = expr[i].replace(lambda expr: isinstance(expr, Float), lambda f: Rational(f))
     
     res = expr.copy()
     for i, e in enumerate(expr):
@@ -52,8 +58,15 @@ def linear_stability_analysis(model, substitutions, linear_system_variables):
         collected = collected.series(eps, 0, 2).removeO()
         order_1_term = collected.coeff(eps, 1)
         res[i] = order_1_term
-    A, rhs = linear_eq_to_matrix(res, linear_system_variables)
+    linearized_system = res
     
+    # equations = [simplify(Eq(linearized_system[i],0)) for i in range(linearized_system.shape[0])]
+    # res = solve(equations[6:], linear_system_variables[6:])
+    # equations = simplify(equations)
+    A, rhs = linear_eq_to_matrix(linearized_system, linear_system_variables)
+    # delete null space
+    mask_nullspace = A.rref()[1]
+    A = A[mask_nullspace, mask_nullspace]
     detA = A.det()
     sol = solve(Eq(detA, 0), omega)
     
