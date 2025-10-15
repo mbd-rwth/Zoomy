94a785fca60f96007bb9ad80e7d5c50db3c4c923
Jonas Buenning <jonas.buenning@t-online.de>
2025-10-15 13:20:21 +0200

    update case to OF12

--- simulations/OF12/hydropower/of_interFoam/clean.sh
+++ simulations/OF12/hydropower/of_interFoam/clean.sh
@@ -1,18 +1,4 @@
-#!/usr/bin/env bash
-mv 0.orig ic
-
-rm -r  0*
-rm -r  1*
-rm -r  2*
-rm -r  3*
-rm -r  4*
-rm -r  5*
-rm -r  6*
-rm -r  7*
-rm -r  8*
-rm -r  9*
-mv ic 0.orig
-cp -r 0.orig 0
-
-rm core*
-rm output*
+rm -r 0.[0-9]*
+rm -r [1-9]*
+rm -r postProcessing
+rm 0/yPlus
